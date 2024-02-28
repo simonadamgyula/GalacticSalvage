34e8505fd73bcdd1fc7879fae2e434d427a64048
simonadamgyula <simon.adam.gyula@students.jedlik.eu>
2024-02-28 21:37:52 +0100

    + meteors spawn and can move - they spawn in the same spot every time - they are too large

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 import pygame
+
+from meteorite import Meteorite
 from player import Player
 
 
@@ -17,9 +19,16 @@ class Game:
             self.screen_resolution[0] // 2, self.screen_resolution[1] // 2, 0
         )
 
+        self.meteorite_spawn_rate: float = 0.5  # hány darab keletkezzen másodpercenként
+        self.meteor_event: int = pygame.event.custom_type()
+        Meteorite.create_random()
+
     def run(self) -> None:
         bg_surf = self.background_generate()
         running: bool = True
+
+        pygame.time.set_timer(self.meteor_event, int(1000 / self.meteorite_spawn_rate))
+
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -27,23 +36,27 @@ class Game:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if pygame.mouse.get_pressed()[0]:
                         self.player.grabber.extend()
+                if event.type == self.meteor_event:
+                    Meteorite.create_random()
 
             keys: pygame.key.ScancodeWrapper = pygame.key.get_pressed()
             self.player.rotate((keys[pygame.K_LEFT] or keys[pygame.K_a]) - (keys[pygame.K_RIGHT] or keys[pygame.K_d]))
-
-            self.screen.blit(bg_surf, (0, 0))
-            self.player.draw(self.screen)
-
-            self.player.update(self.screen)
 
             if keys[pygame.K_UP] or keys[pygame.K_w]:
                 self.player.accelerate()
             pygame.display.update()
 
+            self.screen.blit(bg_surf, (0, 0))
+            self.player.draw(self.screen)
+            Meteorite.update_meteorites(self.screen)
+
+            self.player.update(self.screen)
+
             self.clock.tick(60)
 
         pygame.quit()
 
-    def background_generate(self):
+    @staticmethod
+    def background_generate():
         bg_surf = pygame.image.load("img/background/space.png").convert_alpha()
         return bg_surf
--- meteorite.py
+++ meteorite.py
@@ -1,26 +1,55 @@
+import math
+import random
+
 import pygame
 
 
 class Meteorite:
+    meteorites: list["Meteorite"] = []
+
     def __init__(self, pos: pygame.Vector2, direction: float, speed: float) -> None:
         self.position: pygame.Vector2 = pos
         self.direction: float = direction
         self.velocity: float = speed
 
-        self.rotation_speed: float = 0
+        self.rotation_speed: float = 0.1
         self.rotation: float = 0
 
-        self.image: pygame.Surface = pygame.Surface((100, 100))
-        pygame.draw.circle(self.image, "yellow", (50, 50), 50)
+        self.image: pygame.Surface = pygame.image.load("./img/meteorite/placeholder.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (492 // 2, 507 // 2))
+
+    def update(self) -> None:
+        self.rotate()
+        self.move()
+
+    def move(self) -> None:
+        self.position += pygame.Vector2(
+            math.sin(math.radians(self.direction)),
+            math.cos(math.radians(self.direction))
+        ) * self.velocity
 
     def rotate(self) -> None:
         self.rotation += self.rotation_speed
         self.rotation %= 360
 
     def draw(self, screen: pygame.Surface) -> None:
-        rotated_image: pygame.Surface = pygame.transform.rotate(self.image, self.direction)
+        rotated_image: pygame.Surface = pygame.transform.rotate(self.image, self.rotation)
         rotated_rect: pygame.Rect = rotated_image.get_rect(center=self.image.get_rect(center=self.position).center)
 
         screen.blit(rotated_image, rotated_rect.topleft)
+
+    @staticmethod
+    def create_random() -> None:
+        position: pygame.Vector2 = pygame.Vector2(50, 50)
+        direction: float = random.randrange(0, 360)
+        speed: float = 1
+
+        Meteorite.meteorites.append(Meteorite(position, direction, speed))
+
+    @staticmethod
+    def update_meteorites(screen: pygame.Surface) -> None:
+        for meteorite in Meteorite.meteorites:
+            meteorite.update()
+            meteorite.draw(screen)
 
 
