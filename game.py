8bea9c9c00d371fa5fca5591c745e611b7405bf5
Bella <kovacs.annabella@students.jedlik.eu>
2024-03-06 13:24:07 +0100

    commit

--- game.py
+++ game.py
@@ -31,15 +31,20 @@ class Game:
             "Nyomd meg a szóközt az indításhoz!", True, "white"
         )
         run_rect: pygame.Rect = run_surf.get_rect(center=(800, 470))
+
         # button properties
         button_surf: pygame.Surface = pygame.Surface((100, 100))
         button_surf.fill("white")
         button_rect: pygame.Rect = pygame.Rect(50, 50, 50, 50)
         button_text: pygame.Surface = self.game_font_smaller.render("?", True, "red", "white")
         button_text_rect: pygame.Rect = button_text.get_rect(center=(button_surf.get_width()/2, button_surf.get_height()/2))
-        how_to_play_surf: pygame.Surface = pygame.Surface((400, 400))
-        how_to_play_text: pygame.Rect =
-        how_to_play_rect: pygame.Rect = 
+
+        #how to play properties
+        htp_surf: pygame.Surface = pygame.Surface((400, 400))
+        htp_surf.fill("blue")
+        htp_rect: pygame.Rect = pygame.Rect(200, 200, 200, 200)
+        htp_text: pygame.Surface = self.other_font.render("How to play", True, "green")
+        htp_text_surf: pygame.Rect = htp_rect.get_rect(center=(button_surf.get_width()/2, button_surf.get_height()/2))
 
         running: bool = True
         while running:
