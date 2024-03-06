0cd0a4d611fc06ea789c37b384c4805ff20eea6a
Bella <kovacs.annabella@students.jedlik.eu>
2024-03-06 13:09:37 +0100

    new fonts, button progress

--- game.py
+++ game.py
@@ -19,6 +19,7 @@ class Game:
         self.game_active: bool = False
         self.game_font = pygame.font.Font("font/Beyonders-6YoJM.ttf", 80)
         self.game_font_smaller = pygame.font.Font("font/Beyonders-6YoJM.ttf", 40)
+        self.other_font = pygame.font.Font("font/ninifont-caps.otf", 50)
 
     def run(self) -> None:
         bg_surf: pygame.Surface = self.background_generate()
@@ -31,17 +32,28 @@ class Game:
         )
         run_rect: pygame.Rect = run_surf.get_rect(center=(800, 470))
         # button properties
-        button_surf: pygame.Surface = pygame.Surface((200, 200))
+        button_surf: pygame.Surface = pygame.Surface((100, 100))
         button_surf.fill("white")
         button_rect: pygame.Rect = pygame.Rect(50, 50, 50, 50)
-        button_text: pygame.Surface = self.game_font_smaller.render("?", True, "white", "red")
+        button_text: pygame.Surface = self.game_font_smaller.render("?", True, "red", "white")
         button_text_rect: pygame.Rect = button_text.get_rect(center=(button_surf.get_width()/2, button_surf.get_height()/2))
+        how_to_play_surf: pygame.Surface = pygame.Surface((400, 400))
+        how_to_play_text: pygame.Rect =
+        how_to_play_rect: pygame.Rect = 
 
         running: bool = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    if button_rect.collidepoint(pygame.mouse.get_pos()):
+                        pygame.draw.rect(button_surf, "blue", (200, 200))
+                    else:
+                        pygame.draw.rect(button_surf, (255, 255, 255), (0, 0, 200, 200))
+                        pygame.draw.rect(button_surf, (255, 255, 255), (1, 1, 148, 48))
+                        pygame.draw.rect(button_surf, (0, 0, 0), (1, 1, 148, 1), 2)
+                        pygame.draw.rect(button_surf, (0, 100, 0), (1, 48, 148, 10), 2)
                 # if event.type == pygame.MOUSEBUTTONDOWN:
                 #     if pygame.mouse.get_pressed()[0]:
                 #         self.player.grabber.extend()
@@ -68,14 +80,7 @@ class Game:
                 if keys[pygame.K_SPACE]:
                     self.game_active = True
 
-                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                    if button_rect.collidepoint(pygame.mouse.get_pos()):
-                        pygame.draw.rect(button_surf, "blue", (200, 200))
-                    else:
-                        pygame.draw.rect(button_surf, (255, 255, 255), (0, 0, 200, 200))
-                        pygame.draw.rect(button_surf, (255, 255, 255), (1, 1, 148, 48))
-                        pygame.draw.rect(button_surf, (0, 0, 0), (1, 1, 148, 1), 2)
-                        pygame.draw.rect(button_surf, (0, 100, 0), (1, 48, 148, 10), 2)
+                
 
             button_surf.blit(button_text, button_text_rect)
             self.screen.blit(button_surf, button_rect)
