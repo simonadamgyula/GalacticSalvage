fe4c0324d94d49628096a1460c2f51cd61eaca34
Bella <kovacs.annabella@students.jedlik.eu>
2024-03-21 12:00:17 +0100

    variable name changed, play again text

--- game.py
+++ game.py
@@ -33,10 +33,10 @@ class Game:
         self.game_state: GameState = GameState["MAIN_MENU"]
 
         self.counter_font: pygame.font.Font = pygame.font.Font(None, 200)
-        self.game_font: pygame.font.Font = pygame.font.Font("font/Beyonders-6YoJM.ttf", 80)
-        self.game_font_smaller: pygame.font.Font = pygame.font.Font("font/Beyonders-6YoJM.ttf", 40)
-        self.score_font : pygame.font.Font = pygame.font.Font("font/Beyonders-6YoJM.ttf", 30)
-        self.upgrade_button_font: pygame.font.Font = pygame.font.Font("font/Beyonders-6YoJM.ttf", 20)
+        self.font80: pygame.font.Font = pygame.font.Font("font/Beyonders-6YoJM.ttf", 80)
+        self.font40: pygame.font.Font = pygame.font.Font("font/Beyonders-6YoJM.ttf", 40)
+        self.font30: pygame.font.Font = pygame.font.Font("font/Beyonders-6YoJM.ttf", 30)
+        self.font20: pygame.font.Font = pygame.font.Font("font/Beyonders-6YoJM.ttf", 20)
         self.other_font = pygame.font.Font("font/ninifont-caps.otf", 50)
         self.font_color = pygame.Color(255, 87, 51)
 
@@ -63,25 +63,44 @@ class Game:
         self.points: int = 145
         self.point_multiplier: int = 10
 
-        self.upgrade_manager: UpgradeManager = UpgradeManager({
-            # "max_velocity": 4,
-            # "acceleration": 2,
-            # "grabber_speed": 2,w
-            # "rotation_speed": 2,aa
-            # "can_slow_down": 1,
-            # "grabber length": 4
-        })
+        self.upgrade_manager: UpgradeManager = UpgradeManager(
+            {
+                # "max_velocity": 4,
+                # "acceleration": 2,
+                # "grabber_speed": 2,w
+                # "rotation_speed": 2,aa
+                # "can_slow_down": 1,
+                # "grabber length": 4
+            }
+        )
         self.player.load_upgrades(self.upgrade_manager.get_upgrade_values)
 
-        self.upgrade_button: Button = Button((200, 100), "Upgrade", self.upgrade_button_font,
-                                             (63, 63, 63), "white",
-                                             lambda: self.set_game_state(GameState["UPGRADE_MENU"]),
-                                             lambda: self.game_state == GameState["MAIN_MENU"], center=(800, 750))
-        self.back_button: Button = Button((100, 100), "Back", self.upgrade_button_font,
-                                          (63, 63, 63), "white", lambda: self.set_game_state(GameState["MAIN_MENU"]),
-                                          lambda: self.game_state == GameState["UPGRADE_MENU"], center=(100, 100))
-        self.point_counter: Counter = Counter(self.game_font_smaller, "", (255, 255, 255), center=(300, 100))
-        self.in_game_counter: Counter = Counter(self.score_font, "Jelenlegi pontszámod: ", (255, 255, 255), topleft=(20, 20))
+        self.upgrade_button: Button = Button(
+            (200, 100),
+            "Upgrade",
+            self.font20,
+            (63, 63, 63),
+            "white",
+            lambda: self.set_game_state(GameState["UPGRADE_MENU"]),
+            lambda: self.game_state == GameState["MAIN_MENU"],
+            center=(800, 750),
+        )
+        self.back_button: Button = Button(
+            (100, 100),
+            "Back",
+            self.font20,
+            (63, 63, 63),
+            "white",
+            lambda: self.set_game_state(GameState["MAIN_MENU"]),
+            lambda: self.game_state == GameState["UPGRADE_MENU"],
+            center=(100, 100),
+        )
+        self.point_counter: Counter = Counter(
+            self.font40, "", (255, 255, 255), center=(300, 100)
+        )
+        self.in_game_counter: Counter = Counter(
+            self.font30, "Jelenlegi pontszámod: ", (255, 255, 255), topleft=(20, 20)
+        )
 
         self.upgrade_cards: list[UpgradeCard] = []
         self.new_upgrades()
@@ -89,21 +108,20 @@ class Game:
     def run(self) -> None:
         # main menu
         bg_surf: pygame.Surface = self.background_generate()
-        title_surf: pygame.Surface = self.game_font.render(
+        title_surf: pygame.Surface = self.font80.render(
             "Galactic Salvage", True, "white"
         )
         title_rect: pygame.Rect = title_surf.get_rect(center=(800, 330))
-        run_surf: pygame.Surface = self.game_font_smaller.render(
-            "Nyomd meg a szóközt az indításhoz!", True, "white"
+        run_surf: pygame.Surface = self.font40.render(
+            "Indításhoz nyomd meg a szóközt!", True, "white"
         )
         run_rect: pygame.Rect = run_surf.get_rect(center=(800, 470))
 
         button_surf: pygame.Surface = pygame.Surface((100, 100))
         button_rect: pygame.Rect = pygame.Rect(50, 50, 100, 100)
-          
-        button_text: pygame.Surface = self.game_font_smaller.render("?", True, "white")
-        button_text_rect: pygame.Rect = button_text.get_rect(
 
+        button_text: pygame.Surface = self.font40.render("?", True, "white")
+        button_text_rect: pygame.Rect = button_text.get_rect(
             center=(button_surf.get_width() / 2, button_surf.get_height() / 2)
         )
         screen_note: bool = False
@@ -115,11 +133,18 @@ class Game:
             self.debris_spawn_event, int(1000 / self.debris_spawn_rate)
         )
 
-        pygame.time.set_timer(
-            self.warning_spawn, int(9000))
+        pygame.time.set_timer(self.warning_spawn, int(9000))
 
-        text_surf : pygame.Surface = self.game_font.render("Meghaltál!", True, self.font_color)
-        text_rect : pygame.Rect = text_surf.get_rect(center=(1600 / 2, 900 / 2))
+        text_surf: pygame.Surface = self.font80.render(
+            "Meghaltál!", True, self.font_color
+        )
+        text_rect: pygame.Rect = text_surf.get_rect(center=(1600 / 2, 900 / 2))
+        play_again_surf: pygame.Surface = self.font20.render(
+            "Visszalépéshez nyomd meg szóközt!", True, "white"
+        )
+        play_again_rect: pygame.Rect = play_again_surf.get_rect(
+            center=(1600 / 2, 900 / 2 + 100)
+        )
 
         button_surf.blit(button_text, button_text_rect)
 
@@ -130,7 +155,10 @@ class Game:
                     running = False
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                     self.player.grabber.extend()
-                    if button_rect.collidepoint(pygame.mouse.get_pos()) and self.game_state == GameState["MAIN_MENU"]:
+                    if (
+                        button_rect.collidepoint(pygame.mouse.get_pos())
+                        and self.game_state == GameState["MAIN_MENU"]
+                    ):
                         screen_note = not screen_note
 
                     if self.upgrade_button.rect.collidepoint(pygame.mouse.get_pos()):
@@ -142,7 +170,9 @@ class Game:
                         if card.button.rect.collidepoint(pygame.mouse.get_pos()):
                             success: bool = card.button.click()
                             if success:
-                                self.player.load_upgrades(self.upgrade_manager.get_upgrade_values)
+                                self.player.load_upgrades(
+                                    self.upgrade_manager.get_upgrade_values
+                                )
                 if self.game_state == GameState["IN_GAME"]:
                     if event.type == self.meteor_spawn_event:
                         Meteorite.create_random(self.screen_resolution)
@@ -154,26 +184,28 @@ class Game:
                 if event.type == self.warning_spawn:
                     self.laser.get_pos()
                     self.laser.show_warning = True
-                    pygame.time.set_timer(
-                        self.warning_timer, int(2000), 1)
+                    pygame.time.set_timer(self.warning_timer, int(2000), 1)
                 if event.type == self.warning_timer:
                     self.laser.show_warning = False
                     self.laser.laser_go = True
-                    pygame.time.set_timer(
-                        self.laser_timer, int(2000), 1)
+                    pygame.time.set_timer(self.laser_timer, int(2000), 1)
                 if event.type == self.laser_timer:
                     self.laser.laser_go = False
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
                         if self.game_state == GameState["MAIN_MENU"]:
                             self.set_game_state(GameState["IN_GAME"])
-                        elif self.game_state == GameState["IN_GAME"] and self.player.dead:
+                        elif (
+                            self.game_state == GameState["IN_GAME"] and self.player.dead
+                        ):
                             self.reset()
 
             keys: pygame.key.ScancodeWrapper = pygame.key.get_pressed()
             if self.game_state == GameState["IN_GAME"]:
-                self.player.rotate((keys[pygame.K_LEFT] or keys[pygame.K_a]) -
-                                   (keys[pygame.K_RIGHT] or keys[pygame.K_d]))
+                self.player.rotate(
+                    (keys[pygame.K_LEFT] or keys[pygame.K_a])
+                    - (keys[pygame.K_RIGHT] or keys[pygame.K_d])
+                )
 
                 if keys[pygame.K_UP] or keys[pygame.K_w]:
                     self.player.accelerate()
@@ -186,14 +218,13 @@ class Game:
                 #     f"Pontszámod: {self.player.grabber.points}", 1, self.font_color
                 # )
                 # self.screen.blit(text_points, (20, 20))
-                
+
                 Meteorite.meteorites.update(screen=self.screen)  # type: ignore
                 Debris.debris_group.update(screen=self.screen)  # type: ignore
 
-
-
                 if self.player.dead:
                     self.screen.blit(text_surf, text_rect)
+                    self.screen.blit(play_again_surf, play_again_rect)
 
                 self.current_points += self.player.update() * self.point_multiplier
                 self.player.grabber.check_collect(Debris.debris_group.sprites())  # type: ignore
@@ -203,9 +234,12 @@ class Game:
                     self.player.die()
                 self.player.draw(self.screen)
 
-                if self.player.check_kill_collision(self.laser.kill_rect) and self.laser.laser_go:
+                if (
+                    self.player.check_kill_collision(self.laser.kill_rect)
+                    and self.laser.laser_go
+                ):
                     self.player.die()
-                    
+
                 self.in_game_counter.update(self.current_points)
                 self.in_game_counter.draw(self.screen)
 
@@ -256,20 +290,38 @@ class Game:
             card.draw(self.screen)
 
     def new_upgrades(self) -> None:
-        rand_upgrades: list[tuple[str, int, int]] = self.upgrade_manager.get_random_upgrades(3)
+        rand_upgrades: list[tuple[str, int, int]] = (
+            self.upgrade_manager.get_random_upgrades(3)
+        )
         self.upgrade_cards = []
 
         for index, upgrade in enumerate(rand_upgrades):
-            callables: tuple[Callable[[], typing.Any], Callable[[], bool]] = self.create_callables(upgrade[0])
-            self.upgrade_cards.append(UpgradeCard((200, 300), upgrade[0],
-                                                  upgrade[2], self.upgrade_button_font, "img/debris/satellite.png",
-                                                  "black", "white", callables[0],
-                                                  callables[1], center=(400 * (index + 1), 400)))
+            callables: tuple[Callable[[], typing.Any], Callable[[], bool]] = (
+                self.create_callables(upgrade[0])
+            )
+            self.upgrade_cards.append(
+                UpgradeCard(
+                    (200, 300),
+                    upgrade[0],
+                    upgrade[2],
+                    self.font20,
+                    "img/debris/satellite.png",
+                    "black",
+                    "white",
+                    callables[0],
+                    callables[1],
+                    center=(400 * (index + 1), 400),
+                )
+            )
 
     # don't know why this is needed, but doesn't work without it
-    def create_callables(self, upgrade_name: str) -> tuple[Callable[[], typing.Any], Callable[[], bool]]:
-        return (lambda: self.try_buy(upgrade_name),
-                lambda: self.upgrade_manager.can_buy(upgrade_name, self.points))
+    def create_callables(
+        self, upgrade_name: str
+    ) -> tuple[Callable[[], typing.Any], Callable[[], bool]]:
+        return (
+            lambda: self.try_buy(upgrade_name),
+            lambda: self.upgrade_manager.can_buy(upgrade_name, self.points),
+        )
 
     def try_buy(self, upgrade_name: str) -> None:
         cost: int = self.upgrade_manager.try_buy(upgrade_name, self.points)
--- uielemnts.py
+++ uielemnts.py
@@ -9,16 +9,28 @@ def log(*, center: typing.Any) -> None:
 
 
 class Button:
-    def __init__(self, size: tuple[int, int], text: str, font: pygame.font.Font, bg_color: tuple[int, int, int] | str,
-                 font_color: tuple[int, int, int] | str, function: Callable[[], typing.Any],
-                 active: Callable[[], bool] = lambda: True, usage: int = -1,
-                 disabled_color: tuple[int, int, int] | str = "gray", **position: tuple[int, int]) -> None:
-        self.surface: pygame.Surface = pygame.Surface(size, pygame.SRCALPHA, 32).convert_alpha()
+    def __init__(
+        self,
+        size: tuple[int, int],
+        text: str,
+        font: pygame.font.Font,
+        bg_color: tuple[int, int, int] | str,
+        font_color: tuple[int, int, int] | str,
+        function: Callable[[], typing.Any],
+        active: Callable[[], bool] = lambda: True,
+        usage: int = -1,
+        disabled_color: tuple[int, int, int] | str = "gray",
+        **position: tuple[int, int],
+    ) -> None:
+        self.surface: pygame.Surface = pygame.Surface(
+            size, pygame.SRCALPHA, 32
+        ).convert_alpha()
         log(**position)
         self.rect: pygame.Rect = self.surface.get_rect(**position)
         self.text: pygame.Surface = font.render(text, True, font_color)
         self.text_rect: pygame.Rect = self.text.get_rect(
-            center=(self.surface.get_width() / 2, self.surface.get_height() / 2))
+            center=(self.surface.get_width() / 2, self.surface.get_height() / 2)
+        )
 
         self.bg_color: tuple[int, int, int] | str = bg_color
         self.disabled_color: tuple[int, int, int] | str = disabled_color
@@ -29,7 +41,11 @@ class Button:
         self.active: Callable[[], bool] = active
 
     def draw(self, screen: pygame.Surface) -> None:
-        self.surface.fill(self.bg_color if (self.usage != 0 and self.active()) else self.disabled_color)
+        self.surface.fill(
+            self.bg_color
+            if (self.usage != 0 and self.active())
+            else self.disabled_color
+        )
         self.surface.blit(self.text, self.text_rect)
         screen.blit(self.surface, self.rect)
 
@@ -43,7 +59,12 @@ class Button:
 
 
 class Image:
-    def __init__(self, path: str, size: tuple[int, int] | None = None, **position: tuple[int, int]) -> None:
+    def __init__(
+        self,
+        path: str,
+        size: tuple[int, int] | None = None,
+        **position: tuple[int, int],
+    ) -> None:
         self.surface: pygame.Surface = pygame.image.load(path).convert_alpha()
         if size:
             self.surface = pygame.transform.scale(self.surface, size)
@@ -54,8 +75,13 @@ class Image:
 
 
 class Text:
-    def __init__(self, text: str, font: pygame.font.Font,
-                 color: tuple[int, int, int] | str, **position: tuple[int, int]) -> None:
+    def __init__(
+        self,
+        text: str,
+        font: pygame.font.Font,
+        color: tuple[int, int, int] | str,
+        **position: tuple[int, int],
+    ) -> None:
         self.surface: pygame.Surface = font.render(text, True, color)
         self.rect: pygame.Rect = self.surface.get_rect(**position)
 
@@ -64,8 +90,13 @@ class Text:
 
 
 class Counter:
-    def __init__(self, font: pygame.font.Font, text: str,
-                 color: tuple[int, int, int] | str, **position: tuple[int, int]) -> None:
+    def __init__(
+        self,
+        font: pygame.font.Font,
+        text: str,
+        color: tuple[int, int, int] | str,
+        **position: tuple[int, int],
+    ) -> None:
         self.count: float = 0
 
         self.text = text
@@ -86,20 +117,45 @@ class Counter:
 
 
 class UpgradeCard:
-    def __init__(self, size: tuple[int, int], text: str, price: int, font: pygame.font.Font,
-                 image: str, color: tuple[int, int, int] | str, font_color: tuple[int, int, int] | str,
-                 function: Callable[[], typing.Any], active: Callable[[], bool],
-                 disabled_color: tuple[int, int, int] | str = "gray", **position: tuple[int, int]) -> None:
-        self.surface: pygame.Surface = pygame.Surface(size, pygame.SRCALPHA, 32).convert_alpha()
+    def __init__(
+        self,
+        size: tuple[int, int],
+        text: str,
+        price: int,
+        font: pygame.font.Font,
+        image: str,
+        color: tuple[int, int, int] | str,
+        font_color: tuple[int, int, int] | str,
+        function: Callable[[], typing.Any],
+        active: Callable[[], bool],
+        disabled_color: tuple[int, int, int] | str = "gray",
+        **position: tuple[int, int],
+    ) -> None:
+        self.surface: pygame.Surface = pygame.Surface(
+            size, pygame.SRCALPHA, 32
+        ).convert_alpha()
 
         position: tuple[int, int] = position.get("center", (0, 0))
 
         self.image: Image = Image(image, center=position)
-        self.name: Text = Text(text, font, font_color, center=(position[0], position[1] + 100))
-        self.price_text: Text = Text(f"Price: {price}", font, font_color, center=(position[0], position[1] + 200))
-        self.button: Button = Button((100, 50),
-                                     "Buy", font, color, font_color, function, active=active,
-                                     usage=1, disabled_color=disabled_color, center=(position[0], position[1] + 300))
+        self.name: Text = Text(
+            text, font, font_color, center=(position[0], position[1] + 100)
+        )
+        self.price_text: Text = Text(
+            f"Price: {price}", font, font_color, center=(position[0], position[1] + 200)
+        )
+        self.button: Button = Button(
+            (100, 50),
+            "Buy",
+            font,
+            color,
+            font_color,
+            function,
+            active=active,
+            usage=1,
+            disabled_color=disabled_color,
+            center=(position[0], position[1] + 300),
+        )
 
     def draw(self, screen: pygame.Surface) -> None:
         self.image.draw(screen)
