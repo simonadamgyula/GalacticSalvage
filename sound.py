0f0fb512f89a5180bf14d776a207060264d04bfd
kovacspeter07 <kovacs.peter1@students.jedlik.eu>
2024-04-01 22:23:32 +0200

    typecheck strict

--- sound.py
+++ sound.py
@@ -1,27 +1,31 @@
+from typing import List
+
 import pygame
 
 
 class Sound:
-    def __init__(self):
+    def __init__(self) -> None:
         pygame.mixer.init()
-        self.music_index = 0
-        self.game_music = "sound/game_music.mp3"
-        self.menu_music = "sound/menu_music.mp3"
-        self.all_music = [self.menu_music, self.game_music]
+        self.music_index: int = 0
+        self.game_music: str = "sound/game_music.mp3"
+        self.menu_music: str = "sound/menu_music.mp3"
+        self.all_music: List[str] = [self.menu_music, self.game_music]
         self.music = pygame.mixer.music.load(self.all_music[self.music_index])
 
-        self.laser = pygame.mixer.Sound("sound/laser.mp3")
-        self.button = pygame.mixer.Sound("sound/button.mp3")
-        self.explosion = pygame.mixer.Sound("sound/explosion.mp3")
-        self.warning = pygame.mixer.Sound("sound/warning.mp3")
-        self.wrong_button = pygame.mixer.Sound("sound/wrong_button.mp3")
-        self.extend_arm = pygame.mixer.Sound("sound/extend_arm.mp3")
-        self.collect = pygame.mixer.Sound("sound/collect.mp3")
-        self.catch = pygame.mixer.Sound("sound/catch.mp3")
+        self.laser: pygame.mixer.Sound = pygame.mixer.Sound("sound/laser.mp3")
+        self.button: pygame.mixer.Sound = pygame.mixer.Sound("sound/button.mp3")
+        self.explosion: pygame.mixer.Sound = pygame.mixer.Sound("sound/explosion.mp3")
+        self.warning: pygame.mixer.Sound = pygame.mixer.Sound("sound/warning.mp3")
+        self.wrong_button: pygame.mixer.Sound = pygame.mixer.Sound(
+            "sound/wrong_button.mp3"
+        )
+        self.extend_arm: pygame.mixer.Sound = pygame.mixer.Sound("sound/extend_arm.mp3")
+        self.collect: pygame.mixer.Sound = pygame.mixer.Sound("sound/collect.mp3")
+        self.catch: pygame.mixer.Sound = pygame.mixer.Sound("sound/catch.mp3")
 
         self.enabled: bool = True
 
-        self.all_sound = [
+        self.all_sound: List[pygame.mixer.Sound] = [
             self.laser,
             self.button,
             self.explosion,
