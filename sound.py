23e1101f7d72a8377950d4f0e5e0c42f95111dca
simonadamgyula <simon.adam.gyula@students.jedlik.eu>
2024-04-07 21:09:46 +0200

    settings are now saved

--- sound.py
+++ sound.py
@@ -3,22 +3,26 @@ from typing import List, Any
 import pygame
 
 
-class Sound:
-    __instance: Any = None
+class Sound(object):
+    _instance: Any = None
+    _initialized: bool = False
 
-    def __new__(cls) -> "Sound":
-        if cls.__instance is None:
-            cls.__instance = super(Sound, cls).__new__(cls)
-            return cls.__instance
-        return cls.__instance
+    def __new__(cls, *args, **kwargs) -> "Sound":
+        if cls._instance is None:
+            cls._instance = super(Sound, cls).__new__(cls)
+        return cls._instance
 
     def __init__(self) -> None:
+        if self._initialized:
+            return
+        self._initialized = True
+
         pygame.mixer.init()
         self.music_index: int = 0
         self.game_music: str = "sound/game_music.mp3"
         self.menu_music: str = "sound/menu_music.mp3"
         self.all_music: List[str] = [self.menu_music, self.game_music]
-        self.music = pygame.mixer.music.load(self.all_music[self.music_index])
+        pygame.mixer.music.load(self.all_music[self.music_index])
 
         self.laser: pygame.mixer.Sound = pygame.mixer.Sound("sound/laser.mp3")
         self.button: pygame.mixer.Sound = pygame.mixer.Sound("sound/button.mp3")
@@ -32,7 +36,7 @@ class Sound:
         self.catch: pygame.mixer.Sound = pygame.mixer.Sound("sound/catch.mp3")
 
         self.enabled: bool = True
-        self.music_enabled: bool = True
+        self._music_enabled: bool = True
 
         self.all_sound: List[pygame.mixer.Sound] = [
             self.laser,
@@ -45,12 +49,21 @@ class Sound:
             self.catch,
         ]
 
+    @property
+    def music_enabled(self) -> bool:
+        return self._music_enabled
+
+    @music_enabled.setter
+    def music_enabled(self, value: bool) -> None:
+        self._music_enabled = value
+        self.controll_volume()
+
     def music_index_controll(self) -> None:
         if self.music_index > 1:
             self.music_index = 0
 
     def controll_volume(self) -> None:
-        if self.music_enabled:
+        if self._music_enabled:
             pygame.mixer.music.set_volume(0.3)
         else:
             pygame.mixer.music.set_volume(0)
